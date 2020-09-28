2e73a61abd19ddaaf65719051f4434f394916942
Tanmay Khandelwal <65352484+iamtanmaykh2409@users.noreply.github.com>
2020-09-28 15:49:33 +0530

    Add files via upload

---
+++ PHOTO-GUY/j.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+		$(window).scroll(function(){
+			if($(document).scrollTop()>70){
+				$("nav").addClass("shrink")
+			}
+			else{
+				$("nav").removeClass("shrink")
+			}
+		});
+	});<div class="d-none d-md-block d-lg-none">
+            <div class="slide-box">
+              <img src="images/port (5).jpg" alt="First slide">
+              <img src="images/port (6).jpg" alt="First slide">
+              <img src="images/port (7).jpg" alt="First slide">
+            </div>
+          </div>
---
+++ PHOTO-GUY/main.js
@@ -0,0 +1,70 @@
+//----For NAv scroll---------------------------------------------------------.//---->
+
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      if($(document).scrollTop()>70){
+        $("nav").addClass("shrink")
+      }
+      else{
+        $("nav").removeClass("shrink")
+      }
+    });
+  });
+
+
+/*-------------------------TAB BUTTON PORTFOLIO IMAGES CHanGES---------------------------------------------*/
+
+$( function() {
+  var $grid = $('.grid').isotope({
+    itemSelector: 'article'
+  });
+
+  // filter buttons
+  $('.filters-button-group').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+});
+
+// debounce so filtering doesn't happen every millisecond
+function debounce( fn, threshold ) {
+  var timeout;
+  return function debounced() {
+    if ( timeout ) {
+      clearTimeout( timeout );
+    }
+    function delayed() {
+      fn();
+      timeout = null;
+    }
+    timeout = setTimeout( delayed, threshold || 100 );
+  }
+}
+
+$(window).bind("load", function() {
+  $('#all').click();
+});
+
+
+/*------------------------OWL CAROSEL on images ----------------------------------------------------*/
+
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
